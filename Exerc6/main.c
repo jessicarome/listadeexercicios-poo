79468a7a7882fb56cb86e13910f0a155b72653a2
jessicarome <31140133+jessicarome@users.noreply.github.com>
2017-08-22 19:04:59 -0300

    Update main.c

--- Exerc6/main.c
+++ Exerc6/main.c
@@ -2,39 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
-{
-    int i = 0, cont = 0, j = 0;
-    char dados[50], invertido[50];
+void main(){
+	int j,flag=0; //inicia  flag com 0;
+	char caracter[20];
 
-    printf("Digite a sequencia:\n\n", 136);
-    scanf("%c", &dados);
+	printf("Informe uma palavra: ");
+	scanf("%s",caracter);
+	printf("Contrario: ");
+	for(j=strlen(caracter); j>=0; j--){
+			printf("%c",caracter[j]);
+			if((caracter[strlen(caracter)-1]==caracter[0]) && (caracter[strlen(caracter)-2]==caracter[1]))
+				flag++; //Se a condição for confirmada, flag passa a valer 1;
+	}
+	printf("\n\n");
+	if(flag!=0)
+		printf("PALINDROMO\n\n");
+	else
+		printf("NAO PALINDROMO\n\n");
+}
 
-    while (dados[i] != '\0')
-{
-    cont++;
-    i++;
-}
-    printf("\n\nOrdem inversa:\n\n");
-    for(i = cont; i >= 0; i--)
-{
-    if (dados[i] != '\0')
-{
-    printf("%c", dados[i]);
-    invertido[j] = dados[i];
-    j++;
-}
-}
-    invertido [j] = '\0';
-    if(strcmp(dados, invertido) == 0)
-{
-        printf("\n\nPalindromo!\n\n", 161);
-}
-    else
-{
-    printf("\n\nNao eh um palindromo!\n\n", 198, 130, 161);
-}
-    printf("\n");
-
-return(0);
-}
